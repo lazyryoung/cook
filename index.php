8e3f3b5371936ee189a6b0edf3155a7ee42e343d
lazyryoung <lazyryoung@gmail.com>
2018-05-21 15:35:20 +0900

    test

--- index.php
+++ index.php
@@ -3,11 +3,11 @@ use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 
 require '../../vendor/autoload.php';
-require 'lib/db.php';
+//require 'lib/db.php';
 
 $app = new \Slim\App;
 $app->get('/', function($request, $response, $args) {
-    return $response->withRedirect('/home.php', 301);
+    return $response->withRedirect('home.php', 301);
     //return $response->withStatus(200)->write('Hello world!');
 });
 $app->get('/test', function($request, $response, $args) {
@@ -20,6 +20,6 @@ $app->get('/hello/{name}', function(Request $request, Response $response) {
 });
 
 // abouts Routes
-require 'routes/abouts.php';
+//require 'routes/abouts.php';
 
 $app->run();
